03b02a4033011bb611c1931c34abc5cf17df2235
Denzyl Dick <denzyl@live.nl>
2022-08-16 01:06:48 +0200

    Use log instead of fmt

--- docker.go
+++ docker.go
@@ -7,11 +7,12 @@ import (
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
 	"io"
+	"log"
 	"os"
 )
 
 func spawn(local bool, id string) {
-	fmt.Println("Spawning a new recording container")
+	log.Println("Spawning a new recording container")
 	ctx := context.Background()
 	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 	if err != nil {
